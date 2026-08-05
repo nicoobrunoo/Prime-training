d25ce9cbe7ac8bb16fdfe32926559b7d43d279ee
nicoobrunoo <bruno.nicolas@usal.edu.ar>
2026-08-05 12:29:31 -0300

    Delete assets directory

--- assets/js/core/01-nutrition-shared-state.js
+++
@@ -1,6 +0,0 @@
-var verifiedProductPro = null;
-var mealPhotoDataPro = null;
-var consumeWholeProductPro = false;
-var barcodeReaderPro = null;
-var barcodeControlsPro = null;
-var barcodeLookupBusyPro = false;
